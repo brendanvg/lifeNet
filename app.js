8b6c08f3cc5ecdae030a6388bf472b23202a4812
Brendan Van Gorder <brendanvg10@gmail.com>
2016-04-26 22:39:13 -0400

    need to make edgesDb key: node, value: [[inEdge, inEdge],[outEdge,outEdge]]

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var app = express()
 var body = require('body/any')
 var cors = require('cors')
 var levelup= require('levelup')
-var db = levelup('./mydb20')
+var db = levelup('./mydb28')
+var edgesDb = levelup('./edgesDb1')
+
 var corsOption = {
-	origin: 'http://localhost:5001'
+	origin: 'http://localhost:5003'
 }
 var collect = require('collect-stream')
 
@@ -14,7 +16,9 @@ app.get('/', function(req,res){
 	res.sendfile('index.html')
 })
 
-// db=key=nodeId, value= 'group, x, y, !connected,!,connected'
+
+//value.split('!')  valueArray[1] = in-edge   valueArray[2]=out-edge
+// db=key=nodeId, value= 'group, x, y, !in-edge,in-edge,in-edge! out-edge,out-edge!'
 
 app.get('/loadNodes', cors(corsOption), function(req,res,next){
 	var stream = db.createReadStream()
@@ -37,12 +41,94 @@ app.post('/addNode', cors(corsOption), function(req,res,next){
 	res.end()
 })
 
+
+//need to make edgesDb key: node, value: [[inEdge, inEdge],[outEdge,outEdge]]
 app.post('/addEdge', cors(corsOption), function(req,res,next){
 	body(req,res,function(err,params){
-		console.log('yeaaa',params)
+		console.log('in Node out Node',params)
 
-		db.get
+		edgesDb.get(params.secondNode, function(err,value){
+			if (err) {
+				if (err.notFound){
+					var value = []
+					var updatedValue = value.push(secondNode)
+					console.log('values', updatedValue)
+					edgesDb.put(params.secondNode, updatedValue, function(err){
+						if(err) return console.log(err)
+						console.log('done adding new')
+					})
+				}
+				else {console.log(err)}
+			}
+			else {
+				console.log(value)
+				console.log('yaaa', value.outEdges)
+				console.log('entries',value.entries())
+				var newOutEdges= value.outEdges.push(params.secondNode)
+				edgesDb.put(params.firstNode, newOutEdges, function(err){
+					if(err) return console.log(err)
+					console.log('donnnne updating')
+				})
+			}
+		})
+
+
+		edgesDb.get(params.firstNode, function(err,value){
+			if (err) {
+				if (err.notFound){
+					edgesDb.put(params.firstNode, [params.secondNode], function(err){
+						if(err) return console.log(err)
+						console.log('done adding new')
+					})
+				}
+				else {console.log(err)}
+			}
+			else {
+				console.log(value)
+				console.log('yaaa', value.outEdges)
+				console.log('entries',value.entries())
+				var newOutEdges= value.outEdges.push(params.secondNode)
+				edgesDb.put(params.firstNode, newOutEdges, function(err){
+					if(err) return console.log(err)
+					console.log('donnnne updating')
+				})
+			}
+		})
+		// db.get(params.firstNode, function(err,value){
+		// 	console.log('value of source node', value)
+			
+		// 	if (value.includes('!')){
+		// 		console.log('value of source node before split', value)
+		// 		var valArray = value.split('!')
+		// 		console.log('value of source after split', valArray)
+		// 		var oldOut= valArray[2]
+		// 		var newOut = '!'+valArray[2] + ','+ params.secondNode
+		// 		valArray[2] = newOut
+
+		// 		var newValue = valArray.toString()
+
+
+		// 		db.put(params.firstNode, newValue, function(err){
+		// 			if (err) return console.log(err)
+		// 			console.log('after putting new value of out-edge', newValue)
+		// 		})
+
+		// 		console.log('checking new out', newOut)
+
+		// 	}
+
+		// 	else {
+		// 		var edgeString= '!'+''+'!'+params.secondNode+'!'
+		// 		console.log('woo', value)
+		// 		var newValue= value += edgeString
+		// 		console.log('uhhhhh!!!', newValue)
+		// 		db.put(params.firstNode, newValue, function(err){
+		// 			if (err) return console.log(err)
+		// 		})
+		// 	}
+		// })
 	})
+	res.end()
 })
 
 app.post('/savePositions', cors(corsOption), function(req,res,next){
@@ -62,11 +148,13 @@ app.post('/savePositions', cors(corsOption), function(req,res,next){
 			console.log('valllll array', valArray)
 			valArray[1]=x
 			valArray[2]=y
+			var newString = valArray.toString()
 			console.log('updated x and y', valArray[1], valArray[2])
 
 			// var newValue= value1+','+x+','+y
-
-			db.put(nodeName, valArray, function(err){
+			
+			// db.put(nodeName, valArray, function(err){
+			db.put(nodeName, newString, function(err){
 				if (err) return console.log('err= ',err)
 			})
 
