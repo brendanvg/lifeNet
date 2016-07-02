1f1ff6bf0b06547da9cef8839005cd1c812e4af4
Brendan Van Gorder <brendanvg10@gmail.com>
2016-07-02 01:14:08 -0400

    before data switch.... when adding node should be able to type in multiple groups and multiple networks seperated by comma (then we can parse for it and save the node appropriately)

--- app.js
+++ app.js
@@ -30,7 +30,10 @@ io.on('connection',function(socket){
 
 //BETTER DATA STRUCTURE 
 //key: network, 
-//value: [{
+//value is an array of objects, each object is a node with 
+//specific properties...group property searched to highlight
+//and group like nodes (node can be in more than one group in a network)
+// value: [{
 // 		 node : node,
 //       group: group,
 // 		 position: x, y,
